9cd016f4c321e8628d2721dc6f4ba6752fc3955e
miterfrants <miterfrants@gmail.com>
2014-01-14 23:53:06 +0800

    Place Item

--- Soda/src/com/planb/soda/ListActivity.java
+++ Soda/src/com/planb/soda/ListActivity.java
@@ -7,26 +7,44 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import org.json.*;
 
 import com.loopj.android.http.*;
 
-public class ListActivity extends Activity implements LocationListener {
-	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
-	private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;	
-	public static Location currentLocation=null;
+public class ListActivity extends Activity {
+	
+	private int screenW=0;
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {		
 		super.onCreate(savedInstanceState);
+		screenW=getWindowManager().getDefaultDisplay().getWidth();
 		ActionBar bar=getActionBar();
-		bar.setTitle(getIntent().getStringExtra("title"));
-		
-		if(this.getLocation() !=  null){
+		bar.setDisplayHomeAsUpEnabled(true);
+		bar.setTitle("Soda | "+getIntent().getStringExtra("title"));
+		setContentView(R.layout.activity_list);
+		LocationManager lm=(LocationManager) this.getApplicationContext().getSystemService(LOCATION_SERVICE);
+		Location currentLocation=ShareVariable.getLocation(lm);
+		if(currentLocation !=  null){
 			AsyncHttpClient client = new AsyncHttpClient();
-			client.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+String.valueOf(currentLocation.getLatitude())+","+String.valueOf(currentLocation.getLongitude())+"&radius=500&keyword=%E9%A4%90%E5%BB%B3&sensor=false&key=AIzaSyCYM1UUnXbgP3eD__x2EjIugNOy-vE3McY&rankBy=prominence&types=", new AsyncHttpResponseHandler() {
+			Log.d("test","https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+String.valueOf(currentLocation.getLatitude())+","+String.valueOf(currentLocation.getLongitude())+"&radius=500&keyword=%E9%A4%90%E5%BB%B3&sensor=false&key="+ShareVariable.GOOGLE_KEY+"&rankBy=prominence&types=");
+			client.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+String.valueOf(currentLocation.getLatitude())+","+String.valueOf(currentLocation.getLongitude())+"&radius=500&keyword=%E9%A4%90%E5%BB%B3&sensor=false&key="+ShareVariable.GOOGLE_KEY+"&rankBy=prominence&types=", new AsyncHttpResponseHandler() {
 			    @Override
 			    public void onSuccess(String response) {
-			        Log.d("test","test:"+response);
-			    } 
+			    	try{
+			    		JSONObject res=new JSONObject(response);
+			    		generateList(res);
+			    	}catch(Exception ex){
+			    		Log.d("test","test exception occur:"+ex.getMessage());
+			    	}
+			    }
+			    
+			    @Override
+			    public void onFailure(Throwable e, String response){
+			    	Log.d("test","test async get google api error:"+ e.getMessage());
+			    }
 			});
 		}else{
 			Log.d("test","test: location is null");
@@ -36,47 +54,48 @@ public class ListActivity extends Activity implements LocationListener {
 		//Log.d("test","test2:"+String.valueOf(this.getLoaction().getLatitude()));
 		
 	}
+	public void generateList(JSONObject res){
+	   RelativeLayout rlList =(RelativeLayout) findViewById(R.id.rl_list);
+	   try{
+		   String status =res.getString("status");
+		   if(status.equals("OK")){
+			   JSONArray arrResult=res.getJSONArray("results");
+			   Log.d("test","test:arrResult length:"+arrResult.length());
+			   for(int i=0;i<arrResult.length();i++){
+				   JSONObject item= arrResult.getJSONObject(i);
+				   PlaceItem btn=new PlaceItem(this.getApplicationContext(),this.getWindow().getWindowManager().getDefaultDisplay().getWidth());
+				   RelativeLayout.LayoutParams lpForButton= new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+					lpForButton.height=screenW/2;
+					lpForButton.setMargins(0,i*lpForButton.height, 0, 0);
+					btn.setLayoutParams(lpForButton);
+					btn.bottomLayout.title.setText(item.getString("name"));
+					//dist
+					//JSONObject location=item.getJSONObject("geometry").getJSONObject("location");
+					//btn.getDist(Double.parseDouble(location.getString("lat")), Double.parseDouble(location.getString("lng")));
+					//String urlDist="https://maps.googleapis.com/maps/api/distancematrix/json?origins=%.8F,%.8F&destinations=%.8F,%.8F&mode=walk&language=zh-TW&sensor=false";
+					//btn.bottomLayout.dist.setText("¶ZÂ÷");
+					Log.d("test","test item name:"+item.getString("name"));
+					if(item.has("rating")){
+						btn.rateLayout.setRating((float) item.getDouble("rating")/5);
+						btn.rateLayout.txtRate.setText(String.valueOf(item.getDouble("rating")));
+					}
+					if(item.has("photos")){
+						String photoRef=item.getJSONArray("photos").getJSONObject(0).getString("photo_reference");
+						String url ="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference="+photoRef+"&sensor=false&key="+ShareVariable.GOOGLE_KEY;
+						btn.bg.setTag(url);
+						new DownloadImagesTask().execute(btn.bg);
+					}
+				   rlList.addView(btn);
+			   }
+		   }else{
+			   Log.d("test","test:"+ status);
+		   }
+	   }catch(Exception ex){
+		   Log.d("test","test:exception occur:" + ex.getMessage());
+		   ex.printStackTrace();
+	   }
+	   
+	}
 	
-	public Location getLocation(){
-		LocationManager lm=(LocationManager) this.getApplicationContext().getSystemService(LOCATION_SERVICE);
-		boolean isGPSEnable= lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
-		if(isGPSEnable){
-			
-			lm.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    MIN_TIME_BW_UPDATES,
-                    MIN_DISTANCE_CHANGE_FOR_UPDATES, (LocationListener) this);
-			
-			if (lm != null) {
-				currentLocation= lm
-                        .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            }
-		}
-		return currentLocation;
-	}
 
-	@Override
-	public void onLocationChanged(Location location) {
-		// TODO Auto-generated method stub
-		currentLocation=location;
-	}
-
-	@Override
-	public void onProviderDisabled(String provider) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onProviderEnabled(String provider) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 }
